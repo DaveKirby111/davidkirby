064f80ed9f8d222197e4a0e27d05079b541b6ae0
David Kirby <david@kinetic.com>
2023-05-05 15:33:44 -0500

    working on layout

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -3,14 +3,21 @@ import Nav from "../Nav";
 const Layout = ({ children }) => {
   return (
     <div className="container-fluid d-flex flex-column min-vh-100 flex-wrap p-0 m-0">
-      <header className="d-flex">
-        <h1>Kirby's World</h1>
-        <Nav></Nav>
+      <header>
+        <div className="banner d-flex w-100">
+          <h1>Kirby's World</h1>
+          <Nav></Nav>
+        </div>
       </header>
 
       <main className="flex-grow-1">{children}</main>
 
-      <footer></footer>
+      <footer>
+        <div className="email w-25">
+          <h5 className="text-capitalize">email:</h5>
+          <p>david@kinetic.com</p>
+        </div>
+      </footer>
     </div>
   );
 };
